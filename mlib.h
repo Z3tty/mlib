3c94b3c429e07f0dd6880d1c7240daded07342d2
Mattias <mattias.langseth98@gmail.com>
2018-09-06 14:48:21 +0200

    Updated defines and length function

--- mlib.h
+++ mlib.h
@@ -5,6 +5,9 @@
 #include <math.h>
 #pragma once
 
+#define true	(1==1)
+#define false	(!true)
+
 /*
  * STATE HANDLING
  * -> An enum for different errors, states and returns
@@ -37,14 +40,14 @@ typedef struct node {
  * -> Printing
  */
 // Generic vector math
-extern vec3_t vec3_add(vec3_t v, vec3_t u) 		{ 	return (vec3_t){.x = v.x +u.x, .y = v.y +u.y, .z = v.z +u.z}; 	}
-extern vec3_t vec3_sub(vec3_t v, vec3_t u) 		{ 	return (vec3_t){.x = v.x -u.x, .y = v.y -u.y, .z = v.z -u.z}; 	}
-extern vec3_t vec3_flip(vec3_t v) 				{ 	return (vec3_t){.x = v.x *-1,  .y = v.y *-1,  .z = v.z *-1}; 	}
-extern float vec3_dot(vec3_t v, vec3_t u) 		{	return (float)((v.x *u.x) + (v.y *u.y) + (v.z *u.z));			}
-extern vec3_t vec3_const(int k, vec3_t v)		{	return (vec3_t){.x = v.x *k, .y = v.y *k, .z = v.z *k};			}
-extern float  vec3_len(vec3_t v)				{	return (sqrt(pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2)));			}
-extern void   vec3_print(vec3_t v)				{   printf("[%f, %f, %f]\n", v.x, v.y, v.z);						}
-extern uint_t vec3_compare(vec3_t v, vec3_t u)	{	if(v.x == u.x && v.y == u.y && v.z == u.z) return 1; return 0;	}
+extern vec3_t vec3_add(vec3_t v, vec3_t u) 		{ 	return (vec3_t){.x = v.x +u.x, .y = v.y +u.y, .z = v.z +u.z}; 			}
+extern vec3_t vec3_sub(vec3_t v, vec3_t u) 		{ 	return (vec3_t){.x = v.x -u.x, .y = v.y -u.y, .z = v.z -u.z}; 			}
+extern vec3_t vec3_flip(vec3_t v) 				{ 	return (vec3_t){.x = v.x *-1,  .y = v.y *-1,  .z = v.z *-1}; 			}
+extern float  vec3_dot(vec3_t v, vec3_t u) 		{	return (float)((v.x *u.x) + (v.y *u.y) + (v.z *u.z));					}
+extern vec3_t vec3_const(int k, vec3_t v)		{	return (vec3_t){.x = v.x *k, .y = v.y *k, .z = v.z *k};					}
+extern float  vec3_len(vec3_t v)				{	return (sqrt(pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2)));					}
+extern void   vec3_print(vec3_t v)				{   printf("[%f, %f, %f]\n", v.x, v.y, v.z);								}
+extern uint_t vec3_compare(vec3_t v, vec3_t u)	{	if(v.x == u.x && v.y == u.y && v.z == u.z) return true; return false;	}
 
 /*
  * LINKED LIST OF VEC3 NODES
@@ -54,7 +57,7 @@ extern uint_t vec3_compare(vec3_t v, vec3_t u)	{	if(v.x == u.x && v.y == u.y && 
  *    to manipulate
  */
 
-extern node_t	*create	(vec3_t data)
+extern node_t *create(vec3_t data)
 {
 	// Create a node in a linked list
 	// vec3_t data => Data to be stored in the node
@@ -63,7 +66,7 @@ extern node_t	*create	(vec3_t data)
 	head->data = data;
 	return head;
 }
-extern node_t 	*prepend(node_t *head, vec3_t data)
+extern node_t *prepend(node_t *head, vec3_t data)
 {
 	// Add a node to the front of a linked list
 	// node_t * head => The head of a linked list
@@ -72,7 +75,7 @@ extern node_t 	*prepend(node_t *head, vec3_t data)
 	node->next = head;
 	return node;
 }
-extern node_t 	*append	(node_t *head, vec3_t data)
+extern node_t *append(node_t *head, vec3_t data)
 {
 	// Add a node the the end of a linked list
 	// node_t * head => The head of a linked list
@@ -83,7 +86,7 @@ extern node_t 	*append	(node_t *head, vec3_t data)
 	cursor->next = create(data);
 	return head;
 }
-extern node_t 	*placeat(node_t *head, vec3_t data, size_t pos)
+extern node_t *placeat(node_t *head, vec3_t data, size_t pos)
 {
 	// Add a node at a certain index
 	// node_t * head => The head of a linked list
@@ -98,7 +101,7 @@ extern node_t 	*placeat(node_t *head, vec3_t data, size_t pos)
 		cursor = cursor->next;
 	} printf("Error in extern node_t *placeat(node_t*, vec3_t, size_t) -> ERRNO:\t%d\n", LL_OUT_OF_BOUNDS); return head;
 }
-extern uint_t	search	(node_t *head, vec3_t data)
+extern uint_t search(node_t *head, vec3_t data)
 {
 	// Check if data exists in the list
 	// node_t * head => The head of a linked list
@@ -111,7 +114,7 @@ extern uint_t	search	(node_t *head, vec3_t data)
 		cursor = cursor->next; c++;
 	} return LL_SEARCH_FAIL;
 }
-extern void   	traverse(node_t *head)
+extern void traverse(node_t *head)
 {
 	// Traverse the linked list, printing out the data at each node
 	// node_t * head => The head of a linked list
@@ -121,7 +124,7 @@ extern void   	traverse(node_t *head)
 		cursor = cursor->next;
 	}
 }
-extern void 	lldelete	(node_t *head)
+extern void lldelete(node_t *head)
 {
 	// Delete a linked list once its use is finished
 	// node_t * head => The head of a linked list
@@ -139,13 +142,14 @@ extern void 	lldelete	(node_t *head)
  * NOTICE: Array length checker is NULL terminated. Meaning 0 will be seen as end-of-array
  * Due to the way pointers work, you should self-terminate arrays with a 0/0.0f/0l value
  * Uses generic to switch to the correct function as needed
+ * Since the arrays are null-terminated, we start at 1 tom get the correct length
  */
 size_t length_str(string_t s) 	{ return strlen(s); 						}
-size_t length_int(int i[]) 		{ uint_t c = 0; while(*i++) c++; return c; 	}
-size_t length_flt(float f[])	{ uint_t c = 0; while(*f++) c++; return c; 	}
-size_t length_sht(short s[])	{ uint_t c = 0; while(*s++) c++; return c;	}
-size_t length_lng(long l[])		{ uint_t c = 0; while(*l++) c++; return c;	}
-size_t length_dbl(double d[])	{ uint_t c = 0; while(*d++) c++; return c;	}
+size_t length_int(int i[]) 		{ uint_t c = 1; while(*i++) c++; return c; 	}
+size_t length_flt(float f[])	{ uint_t c = 1; while(*f++) c++; return c; 	}
+size_t length_sht(short s[])	{ uint_t c = 1; while(*s++) c++; return c;	}
+size_t length_lng(long l[])		{ uint_t c = 1; while(*l++) c++; return c;	}
+size_t length_dbl(double d[])	{ uint_t c = 1; while(*d++) c++; return c;	}
 #define length(x) _Generic((x),												\
 								string_t:	length_str,						\
 								int*:		length_int,						\
